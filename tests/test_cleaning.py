05608d96a2a7d90f44e2b03c9d0c228fca10e508
Matt McAleavy <matt.mcaleavy@gmail.com>
2026-04-01 11:16:33 +0100

    starting

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -1,1 +1,15 @@
 # Cleaning tests
+import pytest
+import pandas as pd
+ 
+@pytest.fixture
+def sample_df():
+    """Sample DataFrame for testing."""
+    return pd.DataFrame({
+        'id': [1, 2, 3,3],
+        'name': ['Alice', 'Bob', 'Charlie','Ian']
+    })
+
+def test_remove_duplicates(sample_df):
+    assert sample_df['id'].is_unique
+ 
